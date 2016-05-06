2d531b6d2e0fc10471bb70db536816de84934e17
Patryk Gliszczyński <patryk.gliszczynski94@gmail.com>
2016-05-06 17:26:37 +0200

    Add Writing diseases

--- diagnose.py
+++ diagnose.py
@@ -1,4 +1,5 @@
 import scipy.io
+import scipy.signal
 import numpy
 import matplotlib.pyplot
 import pywt
@@ -6,9 +7,11 @@ import cv2
 import glob, os
 
 def display_plot(y):
-  matplotlib.pyplot.figure()
+  fig, (ax1, ax2) = matplotlib.pyplot.subplots(2, 1)
+  fig.subplots_adjust(hspace = 0.1, left = 0.05, right = 0.95, top = 0.95, bottom = 0.05)
   x = [i for i in range(len(y))]
-  matplotlib.pyplot.plot(x, y)
+  ax1.plot(x, y)
+  ax2.specgram(y, Fs = 1000)
  
 
 def flatten(matrix):
@@ -56,11 +59,24 @@ def get_features():
   return (features, diseases)
 
 
+def write_diseases(diseases):
+  next_value = 1
+  dict = {}
+  f = open('diseases', 'w')
+  for disease in diseases:
+    if (not dict.has_key(disease)):
+      dict[disease] = next_value
+      next_value += 1
+    f.write(str(dict[disease]))
+    f.write('\n')
+  f.close()
+
+
 def main():
   features, diseases = get_features()
-  print(diseases)
   os.chdir("..")
   scipy.io.savemat('features.mat', {'features':features})
+  write_diseases(diseases)
   matplotlib.pyplot.show()
 
 
