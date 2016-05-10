b19512b0b5f0eb668fc4610dd623c37ff2eef7b6
Patryk Gliszczyński <patryk.gliszczynski94@gmail.com>
2016-05-10 12:05:36 +0200

    SVM classification Scikit-learn implementation

--- diagnose.py
+++ diagnose.py
@@ -1,83 +1,27 @@
+#-*- coding: utf-8 -*-
+
 import scipy.io
-import scipy.signal
-import numpy
-import matplotlib.pyplot
-import pywt
-import cv2
-import glob, os
+from sklearn import svm
+import numpy as np
+import matplotlib.pyplot as plt
 
-def display_plot(y):
-  fig, (ax1, ax2) = matplotlib.pyplot.subplots(2, 1)
-  fig.subplots_adjust(hspace = 0.1, left = 0.05, right = 0.95, top = 0.95, bottom = 0.05)
-  x = [i for i in range(len(y))]
-  ax1.plot(x, y)
-  ax2.specgram(y, Fs = 1000)
- 
-
-def flatten(matrix):
-  i = 0
-  flat = []
-  for row in matrix:
-    for el in row:
-      flat.insert(i, el)
-      i += 1
-  return flat
-
-
-def wavelet_transform(matrix):
-  wavelet = []
-  for row in matrix:
-    cA, cD = pywt.dwt(row, 'db1')
-    wavelet.append(cA)
-    wavelet.append(cD)
-  return flatten(wavelet)
-    
-
-def get_disease(file):
-  disease_header = "Reason for admission:"
-  with open(file) as f:
-    lines = f.readlines()
-  
-  for line in lines:
-    if (line.find(disease_header) != -1):
-      disease = line[line.find(disease_header) + len(disease_header) + 1 : line.find("\\") - 1]
-      break
-  return disease
-
-
-def get_features():
-  features = []
-  diseases = []
-  os.chdir("patients")
-
-  for file in glob.glob("*.mat"):
-    matrix = scipy.io.loadmat(file)
-    wavelet = wavelet_transform(matrix['val']) 
-    features.append(wavelet)
-    diseases.append(get_disease(file[:-4] + '.hea'))
-    display_plot(wavelet)
-  return (features, diseases)
-
-
-def write_diseases(diseases):
-  next_value = 1
-  dict = {}
-  f = open('diseases', 'w')
-  for disease in diseases:
-    if (not dict.has_key(disease)):
-      dict[disease] = next_value
-      next_value += 1
-    f.write(str(dict[disease]))
-    f.write('\n')
-  f.close()
+def svm_learning(X, Y):
+  clf = svm.SVC(C=65536, gamma=2.44e-044)
+  clf.fit(X, Y)
+  print(clf.predict([ 0.44067216,  0.45812484,  0.47282945,  0.51718433,  0.53712795,
+        0.5439901 ,  0.57148691,  0.58477731,  0.53844574,  0.4848824 ,
+        0.42371767,  0.39437274,  0.36930259,  0.36319576,  0.36292256,
+        0.32747079,  0.25828682,  0.3636618 ,  0.5969749 ,  1.22962655,
+        1.73652569,  1.32664482,  0.57743304,  0.36848299,  0.28599255,
+        0.31475894,  0.38366971,  0.41998929,  0.42087317,  0.40774348,
+        0.41542523,  0.41012193]))
 
 
 def main():
-  features, diseases = get_features()
-  os.chdir("..")
-  scipy.io.savemat('features.mat', {'features':features})
-  write_diseases(diseases)
-  matplotlib.pyplot.show()
+  features = scipy.io.loadmat("features.mat")['features']
+  diseases = scipy.io.loadmat("diseases.mat")['diseases'][0]
+
+  svm_learning(features, diseases)
 
 
 if __name__ == '__main__':
