2b99828f8908b7434c9d0ce5fc710b91af699d49
Patryk Gliszczyński <patryk.gliszczynski94@gmail.com>
2016-05-17 13:19:59 +0200

    Update features.py

--- features.py
+++ features.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
 
+import math
 import scipy.io
 import scipy.signal
 import numpy
@@ -9,20 +10,13 @@ import glob, os
 import preprocessing
 import numpy as np
 import random
+import sklearn.preprocessing
 
 
-def display_plot(data):
-  fig, (ax1, ax2) = matplotlib.pyplot.subplots(2, 1)
-  fig.subplots_adjust(hspace = 0.1, left = 0.05, right = 0.95, top = 0.95, bottom = 0.05)
-  if (len(np.shape(data)) == 2):
-    for y in data:    
-      ax1.plot(range(0,len(y)), y)
-      ax2.specgram(y, Fs = 1000)
-  else:
-    ax1.plot(range(0,len(data)), data)
-    ax2.specgram(data, Fs = 1000)
- 
-
+'''   Spłaszczenie macierzy do jednego wymiaru
+      @param matrix Macierz wejściowa
+      @return Macierz reprezentowana w jednym wymiarze
+'''
 def flatten(matrix):
   i = 0
   flat = []
@@ -33,6 +27,10 @@ def flatten(matrix):
   return flat
 
 
+'''   Przygotowuje liste chorób dla danego pliku
+      @param file Nazwa pliku .hea
+      @return Choroba odpowiadająca danemu plikowi
+'''
 def get_disease(file):
   disease_header = "Reason for admission:"
   with open(file) as f:
@@ -45,15 +43,58 @@ def get_disease(file):
   return disease
 
 
-def mean(features):
+'''   Wartość średnia
+      @param signal Zbiór sygnałów otrzymanych z transformacji falkowej
+      @return Lista reprezentująca wartość średnią dla każdego punktu
+'''
+def mean(signal):
   mean = []
-  for j in range(len(features[0])):
+  for j in range(len(signal[0])):
     sum = 0
-    for i in range(len(features)):
-      sum = sum + features[i][j]
-    mean.append(sum / len(features))
+    for i in range(len(signal)):
+      sum += signal[i][j]
+    mean.append(sum / len(signal))
   return mean
 
+
+
+'''   Wariancja
+      @param signal Zbiór sygnałów otrzymanych z transformacji falkowej
+      @param mean Wyliczona wartość średnia dla każdego punktu
+      @return Lista reprezentująca wariancje dla każdego punktu
+'''
+def variance(signal, mean):
+  variance = []
+  for j in range(len(signal[0])):
+    sum = 0
+    for i in range(len(signal)):
+      sum += pow(signal[i][j] - mean[j], 2)
+    variance.append(sum / len(signal))
+  return variance
+
+
+
+'''   Odchylenie standardowe 
+      @param variance Wariancja z wcześniej otrzymanego sygnału
+      @return Lista reprezentująca odchylenie standardowe dla każdego punktu
+'''
+def stddev(variance):
+  stddev = []
+  for i in range(len(variance)):
+    stddev.append(math.sqrt(float(variance[i])))
+  return stddev
+
+
+
+'''   Ekstrakcja cech sygnału EKG
+      1) Wyrównanie sygnału 
+      2) Normalizacja sygnału
+      3) Wykrycie uderzeń serca (Pan, Tompkin's AlgorithM)
+      4) Transformacja falkowa
+      5) Wartość średnia transformacji falkowej
+      6) Wariancja transformacji falkowej
+      7) Odchylenie standardowe transformacji falkowej
+'''
 def get_features():
   features = []
   diseases = []
@@ -62,26 +103,45 @@ def get_features():
     print("Processing:\t" + str(dir))
     os.chdir(dir)
     for file in glob.glob("*.mat"):
-      matrix = scipy.io.loadmat(file)
+      matrix = scipy.io.loadmat(file)['val']
+      local_features = []
+      correct = True
+
+      for signal in matrix:
+        # Wyrównanie sygnału
+        mvavg = moving_average(signal, 5001)
  
-      # Pobranie cech sygnału
-      wavelet = preprocessing.getFeature(matrix['val'][0], '0')
+        # Pobranie cech sygnału
+        wavelet = preprocessing.getFeature(mvavg, '0')
 
-      # Wartość średnia z otrzymanego wavelet'u
-      mean_val = mean(wavelet)
+        # Wartość średnia z otrzymanego wavelet'u
+        meanV = mean(wavelet)
+        if (len(meanV) != 32): 
+          correct = False
+          break
 
+        # Wariancja z otrzymanego wavelet'u
+        varianceV = variance(wavelet, meanV)
+
+        # Odchylenie standardowe z otrzymanego wavelet'u
+        stddevV = stddev(varianceV)
+
+        # Transformata Fouriera przeprowadzona po średniej wartości transformaty falkowej
+        fftV = fourier_transform(meanV)
+
+        # Dopisanie wyliczonych cech z wavelet'u
+        local_features.append(meanV)
+        #local_features.append(varianceV)    # Redundancja cech? 
+        local_features.append(stddevV)
+        local_features.append(fftV)
+      
       # Sprawdzenie czy powiodła się ekstrakcja cech
-      if (len(mean_val) != 32): continue
-      
-      # Dopisanie uśrednionych cech do macierzy cech
-      features.append(mean_val)
-   
-      # Dopisanie choroby do listy
-      diseases.append(get_disease(file[:-4] + '.hea'))
-      
-      # Wyświetlenie otrzymanych danych na wykresach   
-      #display_plot(mean_val)
-      #display_plot(features)
+      if (correct == True):
+        # Spłaszczenie i dopisane lokalnych cech do macierzy cech
+        features.append(np.asarray(local_features).reshape(-1))
+
+        # Dopisanie choroby do listy
+        diseases.append(get_disease(file[:-4] + '.hea'))
 
     os.chdir("..")
   return (features, diseases)
@@ -99,6 +159,21 @@ def fuzzy_values(diseases):
   return fuzzy_diseases
 
 
+def moving_average(signal, window=101):
+  weights = np.repeat(1.0, window) / window
+  smas = np.convolve(signal, weights, 'valid')
+  signal = signal[(window - 1) / 2 : len(signal) - ((window - 1) / 2)]
+  new_signal = []
+  for index, x in enumerate(signal):
+    new_signal.append(signal[index] - smas[index])
+  return new_signal
+
+
+def fourier_transform(signal):
+  signalFFT = np.abs(np.fft.fft(signal))**2
+  return signalFFT
+  
+
 def main():
   features, diseases = get_features()
   diseases = fuzzy_values(diseases)
@@ -109,8 +184,9 @@ def main():
 
 
 ########################################################################
-###################  JUST TO VISUALIZE OUR FEATURES  ################### 
+#######################  FEATURES VISUALIZATION  ####################### 
 ########################################################################
+
 def features_presentation(number_of_plots):
   os.chdir("patients")
   plot_number = 0
@@ -123,14 +199,40 @@ def features_presentation(number_of_plots):
     file = random.choice(files_list)
     print("Processing:\t" + str(file))
    
-    matrix = scipy.io.loadmat(file)
+    matrix = scipy.io.loadmat(file)['val']
+    print (np.shape(matrix))
     plot_number = plot_number + 1
+    signal = matrix[0]
+    signal = moving_average(signal, 5001)
+    #signal = sklearn.preprocessing.normalize(signal)
 
+    #matplotlib.pyplot.plot(range(0, len(signal)), signal)
+    #matplotlib.pyplot.figure()
+    #matplotlib.pyplot.plot(range(0, len(mean_avg)), mean_avg)
+    #matplotlib.pyplot.show()
+    
     # Pobranie cech sygnału
-    wavelet = preprocessing.getFeature(matrix['val'][0], '0')
+    wavelet = preprocessing.getFeature(signal, '0')
+    wavelet = sklearn.preprocessing.normalize(wavelet)
 
+    #print(np.shape(wavelet))
+    #wavelet = sklearn.preprocessing.normalize(wavelet)
+    #print(np.shape(wavelet))
     # Wartość średnia z otrzymanego wavelet'u
-    mean_val = mean(wavelet)
+    meanV = mean(wavelet)
+    varianceV = variance(wavelet, meanV)
+    stddevV = stddev(varianceV)
+    fftV = fourier_transform(meanV)
+    
+
+    features = []
+    features.append(meanV)
+    #features.append(varianceV)
+    features.append(stddevV)
+    features.append(fftV)
+    features = np.asarray(features).reshape(-1)
+    print(np.shape(meanV))
+    print(np.shape(features))
       
     # Wyświetlenie otrzymanych danych na wykresach   
     fig = matplotlib.pyplot.figure()
@@ -142,14 +244,14 @@ def features_presentation(number_of_plots):
     ax3 = matplotlib.pyplot.subplot2grid((3, 2), (2, 0))
     ax4 = matplotlib.pyplot.subplot2grid((3, 2), (2, 1))
     ax1.set_title(str(get_disease(file[:-4] + '.hea')))
-    ax1.set_xlim([0, len(mean_val) - 1])
-    ax2.set_xlim([0, len(mean_val) - 1])
+    ax1.set_xlim([0, len(features) - 1])
+    ax2.set_xlim([0, len(features) - 1])
       
     for y in wavelet:      
       ax1.plot(range(0,len(y)), y)
-      ax3.specgram(y, Fs = 1000, cmap=matplotlib.pyplot.cm.gist_rainbow)
-    ax2.plot(range(0, len(mean_val)), mean_val)
-    ax4.specgram(mean_val, Fs = 1000, cmap=matplotlib.pyplot.cm.gist_rainbow)
+      ax3.specgram(y, Fs = 32, cmap=matplotlib.pyplot.cm.gist_rainbow)
+    ax2.plot(range(0, len(features)), features)
+    ax4.specgram(features, Fs = 32, cmap=matplotlib.pyplot.cm.gist_rainbow)
 
     os.chdir("..")
   matplotlib.pyplot.show()
@@ -157,5 +259,5 @@ def features_presentation(number_of_plots):
 
 
 if __name__ == '__main__':
-  #main()
-  features_presentation(6)
+  main()
+  #features_presentation(3)
